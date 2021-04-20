4dc99ec87eb604aa3b4339307fd46cffb3c64a35
Owen Synge <os@nect.com>
2021-04-20 16:10:42 +0200

    small fix

--- flowpipe/graph.py
+++ flowpipe/graph.py
@@ -306,8 +306,9 @@ class Graph(object):
         def node_runner(node, parent_span):
             """Run a node's evaluate method and return the node."""
             with opentracing.tracer.scope_manager.activate(parent_span, finish_on_close=True):
-                with opentracing.tracer.start_active_span('child'):
-                    node.evaluate()
+                with opentracing.tracer.start_active_span('node in thread') as scope:
+                    with opentracing.tracer.scope_manager.activate(scope.span, True):
+                        node.evaluate()
             return node
 
         running_futures = {}
@@ -320,7 +321,7 @@ class Graph(object):
                 not_submitted = []
                 for node in nodes_to_evaluate:
                     if not any(n.is_dirty for n in node.upstream_nodes):
-                        with opentracing.tracer.start_active_span('parent') as scope:
+                        with opentracing.tracer.start_active_span('node thread submit') as scope:
                             fut = executor.submit(node_runner, node, scope.span)
                         running_futures[node.name] = fut
                     else:
